be0aa6b9016c2135a0e8a0f08a8a5cba72537f94
Tushar <tusharpersai@gmail.com>
2025-09-14 16:55:53 +0530

    removed-create.tsx

--- src/components/create.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Button } from "./ui/button";
-import { queueSong } from "~/actions/generation";
-
-function CreateSong() {
-  return <Button onClick={queueSong}>Generate Song</Button>;
-}
-
-export default CreateSong;
